69f4bb55bd8d104c66959452942a47e8a00e65a2
Crew Spence <csspence123@gmail.com>
2020-07-28 05:53:33 -0500

    Created function stub for toy problem.

---
+++ naive_string_search_udemy.js
@@ -0,0 +1,9 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass - Naive String Search
+
+Create a function naiveSearch that accepts two strings.  It should loop over the first string and return the occurrences of the second string.
+*/
+
+const naiveSearch = (str1, str2) => {
+  
+}
