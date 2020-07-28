822cfec65e05c7c73d30cbe1560f12f98da10979
Crew Spence <csspence123@gmail.com>
2020-07-28 06:01:25 -0500

    Wrote working solution.

--- naive_string_search_udemy.js
+++ naive_string_search_udemy.js
@@ -5,5 +5,25 @@ Create a function naiveSearch that accepts two strings.  It should loop over the
 */
 
 const naiveSearch = (str1, str2) => {
-  
+  let count = 0;
+  for(let i = 0; i < str1.length; i++) {
+    if(str1[i] === str2[0]) {
+      let index = i + 1;
+      for(let h = 1; h < str2.length; h++) {
+        if(str2[h] === str1[index] && h < str2.length - 1) {
+          index++;
+          continue;
+        } else if(str2[h] === str1[index] && h === str2.length - 1) {
+          count++;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  return count;
 }
+
+// naiveSearch('jkjakdjiekmkjomgaijdkjjjenndomguujdjomgomg', 'omg'); // 4
+// naiveSearch('jakdjfkjakjk4ejkjkemckjukjdkjuuujk', 'omg'); // 0
